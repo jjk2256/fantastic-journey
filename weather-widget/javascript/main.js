6fe12fd4dc2c0e5907f80e35a679e72d3a403114
jjk2256 <jjk2256@columbia.edu>
2024-06-18 15:52:16 -0400

    Data and APIs # replace Create index.html
    Interactive Web submission # replace Description

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,7 +1,24 @@
 console.log("this works");
 const API_KEY = config.WEATHER_API_KEY;
+<body>
+  ...
+  
+  <script src="config.js"></script>  
+  <script src="javascript/main.js"></script>
+</body>
+console.log("Hello, World!");
 // using concatenation
 "https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode
 fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
   .then(response => response.json())
   .then(data => console.log(data));
+  function getWeatherData() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // store the data in a variable of our choosing
+        let local_weather_data = data;
+        // log the data to the browser console
+        console.log(local_weather_data);
+      });
+    }
