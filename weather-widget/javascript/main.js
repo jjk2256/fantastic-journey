c5a968df8ce6e2bdb61317fbb3abe609338af00f
jjk2256 <jjk2256@columbia.edu>
2024-06-19 14:30:34 -0400

    Data and APIs
    Interactive Web submission

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -3,7 +3,7 @@ const API_KEY = config.WEATHER_API_KEY;
 function getWeatherData() {
 // using concatenation
 "https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode
-fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=1d3af814e0c344bc1b4d8973771e6c37`)
+fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=API_KEY`)
   .then(response => response.json())
   .then(data => console.log(data));
 }
