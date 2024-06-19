0354f75f4be09ffa11fcbb44301feb82f2f5cba8
jjk2256 <jjk2256@columbia.edu>
2024-06-19 15:08:31 -0400

    Events and UIs
    Interactive Web submission

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,9 +1,22 @@
-console.log("this works");
-const API_KEY = config.WEATHER_API_KEY;
-function getWeatherData() {
-// using concatenation
-"https://api.openweathermap.org/data/2.5/weather?zip=" + zipcode
-fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=API_KEY`)
-  .then(response => response.json())
-  .then(data => console.log(data));
-}
+let input = document.querySelector(".zipcode");
+let btn = document.querySelector(".search-button");
+
+getWeatherData = (zip) => {
+  let API_KEY = config.WEATHER_API_KEY;
+  let API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
+
+  fetch(API_ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      let local_weather_data = data;
+        console.log(local_weather_data)
+    })
+  }
+
+const getZipCode = (e) => {
+  e.preventDefault();
+  let ZIP_CODE = input.value;
+  getWeatherData(ZIP_CODE);
+};
+
+btn.addEventListener("click", getZipCode);
